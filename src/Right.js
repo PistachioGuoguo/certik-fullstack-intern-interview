80902c5435a08a8aea88cfb964dda6a4e9b00e56
Pistachio Guoguo <sangzizhou@gmail.com>
2021-03-26 15:54:34 -0500

    a simple gauge completed

--- src/Right.js
+++ src/Right.js
@@ -7,6 +7,7 @@ const Right = (props) => {
     const numStaticIssues = indicators[0]['issues'];
     const numChecks = indicators[0]['checks'];
 
+    const scoreStatic = indicators[0]['score'];
     const scoreOnchain = indicators[1]['score'];
     const scoreSentiment = indicators[2]['score'];
 
@@ -16,47 +17,93 @@ const Right = (props) => {
 
     const rate = (score) => {
         if (score >= 90){
-            return "Excellent";
+            return {color:"#40B884", grade:"Excellent"};
         }else if(score >= 80){
-            return "Good";
+            return {color:"#E1AA4C", grade:"Good"};
         }else{
-            return "Average";
+            return {color:"#E75252", grade:"Average"};
         }
     }
+
+    const rateStatic = rate(scoreStatic);
+    const rateOnChain = rate(scoreOnchain);
+    const rateVolatility = rate(scoreVolatility);
+    const rateRisk = rate(scoreRisk);
+    const rateSentiment = rate(scoreSentiment);
+
+
+    const staticGaugeStyle = {
+        "width": "100px",
+        "--rotation": (scoreStatic * 1.8) + "deg",
+        "--color": rateStatic.color,
+        "--background": "#e9ecef"
+    };
 
 
     return ( 
        <div className="card-list">
 
             <div className="card-item">
-                <h5>Static Analysis</h5>
-                <p> {numStaticIssues} issues selected out of {numChecks} vulnerability and security checks</p>
+
+                <div className="card-text">
+                    <h5>Static Analysis</h5>
+                    <p> <text style={{color:rateStatic.color}}> {numStaticIssues} </text> issues selected out of {numChecks} vulnerability and security checks</p>
+
+                </div>
+
+                <div className="card-gauge">
+                    <div className="gauge" style={staticGaugeStyle}>
+                        <div className="percentage"></div>
+                        <div className="mask"></div>
+                        <span className="value">{scoreStatic}</span>
+                    </div>
+                </div> 
+
             </div>
 
             <div className="card-item">
-                <h5>On-chain Monitoring</h5>
-                <p> {rate(scoreOnchain)} based on real-time transactional tracking systems </p>
+
+                <div className="card-text">
+                    <h5>On-chain Monitoring</h5>
+                    <p> <text style={{color:rateOnChain.color }}> {rateOnChain.grade} </text> based on real-time transactional tracking systems </p>
+                </div>
+              
             </div>
 
             <div className="card-item">
-                <h5>Social Sentiment</h5>
-                <p> {scoreSentiment} based on social monitoring and sentiment analysis </p>
+
+                <div className="card-text">
+                    <h5>Social Sentiment</h5>
+                    <p><text style={{color:rateSentiment.color }}> {rateSentiment.grade} </text> {scoreSentiment} based on social monitoring and sentiment analysis </p>
+                </div>
+               
             </div>
 
             <div className="card-item">
-                <h5>Governance & Autonomy</h5>
-                <p> {1} security-type certificate found on <a href="http://google.com/">Certik Chain</a></p>
+                <div className="card-text">
+                    <h5>Governance & Autonomy</h5>
+                    <p> <text style={{color:"#40B884"}}>{1}</text>  security-type certificate found on <a href="http://google.com/">Certik Chain</a></p>
+                </div>
+               
             </div>
 
             <div className="card-item">
-                <h5>Market Volatility</h5>
-                <p> {rate(scoreVolatility)} based on indicators over trading volume/liquidity/depth </p>
+                <div className="card-text">
+                    <h5>Market Volatility</h5>
+                    <p> <text style={{color:rateVolatility.color }}> {rateVolatility.grade} </text> based on indicators over trading volume/liquidity/depth </p>
+                </div>
+               
             </div>
 
+    
             <div className="card-item">
-                <h5>Risk Assessment</h5>
-                <p> {rate(scoreRisk)} based on {numRiskIssues} safety and hazard evaluations </p>
+                <div className="card-text">
+                    <h5>Risk Assessment</h5>
+                    <p> <text style={{color:rateRisk.color}}> {rateRisk.grade} </text> based on {numRiskIssues} safety and hazard evaluations </p>
+                </div>
+               
             </div>
+
 
        </div>     
     );
